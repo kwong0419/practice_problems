b338edb66a52b661fbf05b048dbec45e43032196
Kevin Wong <kevinwong@pursuit.org>
2021-01-22 17:57:27 -0500

    update balanced_parenthesis problem with leetcode Valid Parenthesis, use map implementation

--- balanced_parenthesis.js
+++ balanced_parenthesis.js
@@ -54,6 +54,84 @@ const isBalancedParenthesis = (str) => {
     }
 }
 
-console.log(isBalancedParenthesis(""))
-console.log(isBalancedParenthesis("()()()"))
-console.log(isBalancedParenthesis("(()"))
+// console.log(isBalancedParenthesis(""))
+// console.log(isBalancedParenthesis("()()()"))
+// console.log(isBalancedParenthesis("(()"))
+
+
+
+//
+// LeetCode - 20. Valid Parentheses
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+ 
+
+// Example 1:
+
+// Input: s = "()"
+// Output: true
+// Example 2:
+
+// Input: s = "()[]{}"
+// Output: true
+// Example 3:
+
+// Input: s = "(]"
+// Output: false
+// Example 4:
+
+// Input: s = "([)]"
+// Output: false
+// Example 5:
+
+// Input: s = "{[]}"
+// Output: true
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 104
+// s consists of parentheses only '()[]{}'.
+
+
+const map = {
+    "(": ")",
+    "[": "]",
+    "{": "}"
+}
+
+const isValid = (s) => {
+    const stack = [];
+
+    for(let i = 0; i < s.length; i++){
+        var el = s[i];
+
+        if(map[el]){
+            stack.push(map[el]);
+        } else {
+            if(el !== stack.pop()){
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+}
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+console.log(isValid("()"))
+// true
+console.log(isValid("()[]{}"))
+// true
+console.log(isValid("(]"))
+// false
+console.log(isValid("([)]"))
+// false
+console.log(isValid("{[]}"))
+// true
