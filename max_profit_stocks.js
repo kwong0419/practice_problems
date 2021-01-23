8b4f87322b95e63af4c14f267422a41b7a178fca
Kevin Wong <kevinwong@pursuit.org>
2021-01-22 19:01:16 -0500

    add time complexity of maxProfit problem

--- max_profit_stocks.js
+++ max_profit_stocks.js
@@ -47,6 +47,8 @@ const maxProfit = (prices) => {
     }
     return maxProfit
 }
+// Time Complexity: O(n)
+
 
 console.log(maxProfit([7, 1, 5, 3, 6, 4])); // => 5
 console.log(maxProfit([7, 6, 4, 3, 1])); // => 0
