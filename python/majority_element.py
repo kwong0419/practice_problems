6d27635f633df2a3de76d03b91e3742ad20cea4e
Kevin Wong <kevin.wong954@gmail.com>
2025-12-29 20:36:17 -0500

    complete majority_element leetcode problem in py

---
+++ python/majority_element.py
@@ -0,0 +1,41 @@
+# Given an array nums of size n, return the majority element.
+
+# The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+# Example 1:
+
+# Input: nums = [3,2,3]
+# Output: 3
+# Example 2:
+
+# Input: nums = [2,2,1,1,1,2,2]
+# Output: 2
+ 
+
+# Constraints:
+
+# n == nums.length
+# 1 <= n <= 5 * 10^4
+# -10^9 <= nums[i] <= 10^9
+# The input is generated such that a majority element will exist in the array.
+ 
+
+# Follow-up: Could you solve the problem in linear time and in O(1) space?
+
+def majority_element(nums):
+    count = 0
+    candidate = None
+    
+    for num in nums:
+        if count == 0:
+            candidate = num
+        count += (1 if num == candidate else -1)
+    
+    return candidate
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+# Example Usage:
+print(majority_element([3, 2, 3])) # 3
+print(majority_element([2, 2, 1, 1, 1, 2, 2])) # 2
