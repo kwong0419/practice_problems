27e9fab1d3f593b83d84da357bba090233526cf6
Kevin Wong <kevin.wong954@gmail.com>
2026-02-24 19:36:08 -0500

    complete swap_nodes_in_pairs leetcode problem in py

---
+++ python/swap_nodes_in_pairs.py
@@ -0,0 +1,55 @@
+# Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
+
+
+
+# Example 1:
+
+# Input: head = [1,2,3,4]
+
+# Output: [2,1,4,3]
+
+# Explanation:
+
+
+
+# Example 2:
+
+# Input: head = []
+
+# Output: []
+
+# Example 3:
+
+# Input: head = [1]
+
+# Output: [1]
+
+# Example 4:
+
+# Input: head = [1,2,3]
+
+# Output: [2,1,3]
+
+
+
+# Constraints:
+
+# The number of nodes in the list is in the range [0, 100].
+# 0 <= Node.val <= 100
+
+def swapPairs(head):
+    if not head or not head.next:
+        return head
+    newHead = head.next
+    head.next = swapPairs(head.next.next)
+    newHead.next = head
+    return newHead
+
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+
+# Example Usage:
+print(swapPairs([1, 2, 3, 4])) # [2, 1, 4, 3]
+print(swapPairs([])) # []
+print(swapPairs([1])) # [1]
+print(swapPairs([1, 2, 3])) # [2, 1, 3]
