df90cb2d46da1cf2f7b7560fe342378b7aac6199
Kevin Wong <kevin.wong954@gmail.com>
2026-04-24 16:59:48 -0400

    complete arranging_coins leetcode problem in py

---
+++ python/arranging_coins.py
@@ -0,0 +1,42 @@
+# You have n coins and you want to build a staircase with these coins. The staircase consists of k rows where the ith row has exactly i coins. The last row of the staircase may be incomplete.
+
+# Given the integer n, return the number of complete rows of the staircase you will build.
+
+ 
+
+# Example 1:
+
+
+# Input: n = 5
+# Output: 2
+# Explanation: Because the 3rd row is incomplete, we return 2.
+
+# Example 2:
+
+
+# Input: n = 8
+# Output: 3
+# Explanation: Because the 4th row is incomplete, we return 3.
+ 
+
+# Constraints:
+
+# 1 <= n <= 2^31 - 1
+
+def arrange_coins(n):
+    left = 0
+    right = n
+    while left <= right:
+        mid = (left + right) // 2
+        if mid * (mid + 1) // 2 <= n:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return right
+
+# Time Complexity: O(log n)
+# Space Complexity: O(1)
+
+# Example Usage:
+print(arrange_coins(5)) # 2
+print(arrange_coins(8)) # 3
